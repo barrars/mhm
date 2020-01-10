78a9843012647ccf675be6f9055a1fb13ccec80e
scott barrar <barrars@gmail.com>
2020-01-09 21:54:04 -0800

    fix weather icon scape and wind speed

--- mhm.js
+++ mhm.js
@@ -3,9 +3,6 @@ const cheerio = require('cheerio')
 const axios = require('axios')
 const ninjadb = require('ninjadb')
 const conditions = ninjadb.create('conditions')
-
-
-
 const cams = {
   vista: 'https://www.skihood.com/cams/vista',
   base: 'https://www.skihood.com/cams/base',
@@ -14,17 +11,15 @@ const cams = {
   sahale: 'https://www.skihood.com/cams/sahale',
   top: 'https://www.skihood.com/cams/top'
 }
-
-
 const mhm = () => {
   setInterval(() => {
 		axios.get('https://www.skihood.com/en/the-mountain/conditions').then((response) => {
 
 	const $ = cheerio.load(response.data)
-	let temp = $('#ctl17 > section > section > div > div.conditions-snapshot > div.conditions-glance-widget.conditions-current > dl > dd.reading.temperature').text()
-	let wind = $('#ctl17 > section > section > div > div.conditions-snapshot > div.conditions-glance-widget.conditions-at-elevations > div:nth-child(1) > dl > dd.reading.windspeed').text()
-	let condition = $('#ctl17 > section > section > div > div.conditions-snapshot > div.conditions-glance-widget.conditions-current > dl > dd.reading.conditions').text()
-	// console.log(temp, wind, condition)
+	let temp = $('dd.reading.temperature').text()
+	let wind = $('dd.reading.windspeed').text() || '0 mph'
+	let condition = $('dd.reading.conditions').attr('data-conditions')
+	console.log(temp, wind, condition)
 	conditions.push({
 		date:new Date().toLocaleString(),
 		temp,
@@ -39,7 +34,7 @@ const mhm = () => {
       const output = `./public/images/${key}/${date.valueOf()}.jpg`
       wget.download(src, output)
     }
-  }, 300000)
+  }, 3000)
 }
 
 module.exports = mhm()
